nrc-cnrc/EGSnrc

commit 50e0d336ef1519bb04c4ba36714f24b07d79be9b

Fix #205: radiative splitting reset in egs_chamber
Reset the nbr_split radiative splitting number to its input value at the
start of every shower. Previously, nbr_split was set to 1 after the
first photon interaction (when cross-section enhancement was turned on),
effectively turning off radiative splitting for the rest of the
simulation.

HEN_HOUSE/user_codes/egs_chamber/egs_chamber.cpp
@@ -659,6 +659,7 @@ private:
 
     EGS_Float        fsplit;    // photon splitting number
     EGS_Float        fspliti;   // inverse photon splitting number
+    int              csplit;    // radiative splitting number
 
     /*! Range rejection flag
       If set to 0, no range rejection is used
@@ -939,8 +940,8 @@ int EGS_ChamberApplication::initScoring() {
         //
         // ******** radiative event splitting
         //
-         int csplit=1;
-         if( !vr->getInput("radiative splitting", csplit) && csplit > 1) {
+        csplit=1;
+        if( !vr->getInput("radiative splitting", csplit) && csplit > 1) {
             egsInformation("\n => initScoring: splitting radiative events %d times ...\n", csplit);
            the_egsvr->nbr_split = csplit;
         }
@@ -2201,6 +2202,7 @@ int EGS_ChamberApplication::simulateSingleShower() {
 
     last_case = current_case;
     EGS_Vector x,u;
+    the_egsvr->nbr_split = csplit;
     current_case = source->getNextParticle(rndm,p.q,p.latch,p.E,p.wt,x,u);
     //egsInformation("Got particle: q=%d E=%g wt=%g latch=%d x=(%g,%g,%g) u=(%g,%g,%g)\n",p.q,p.E,p.wt,p.latch,x.x,x.y,x.z,u.x,u.y,u.z);
     int err = startNewShower(); if( err ) return err;

commit f5dc2af7d681f452a511e6f2e59affb316ade85c

Fix radiative splitting csplit initialization
Initialize the csplit variable to 1 by default in the egs_chamber
application constructor; csplit is used to read in the radiative
splitting number from the input file.

Previously, csplit was only initialized if the input contained a
variance reduction input block. This caused a serious bug to be
introduced in commit 50e0d33, whereby the_egsvr->nbr_split was set to
the arbitrary (uninitialized) csplit number in the shower loop when
there was no variance reduction input block. If this number happened to
be a large positive integer (about half the time), any radiative event
immediately exceeded the stack size and aborted the simulation.

This probably went unnoticed for so long because one rarely runs
egs_chamber without variance reduction.

HEN_HOUSE/user_codes/egs_chamber/egs_chamber.cpp
@@ -548,7 +548,7 @@ public:
 	cgeoms(0), nsubgeoms(0), check_for_subreg(0), is_subgeomreg(0), subgeoms(0),
 	container(0), container2(0), container3(0), save_dose(0), silent(0) ,
         iso_pu_flag(0), cav_pu_flag(0), iso_pu_do_shift(0), cav_pu_do_shift(0),pu_flag(0),
-        McasePerPos(0), NposPerSample(0), onegeom(0) {  };
+        McasePerPos(0), NposPerSample(0), onegeom(0), csplit(1) {  };
 
     /*! Destructor.  */
     ~EGS_ChamberApplication() {
@@ -940,7 +940,6 @@ int EGS_ChamberApplication::initScoring() {
         //
         // ******** radiative event splitting
         //
-        csplit=1;
         if( !vr->getInput("radiative splitting", csplit) && csplit > 1) {
             egsInformation("\n => initScoring: splitting radiative events %d times ...\n", csplit);
            the_egsvr->nbr_split = csplit;