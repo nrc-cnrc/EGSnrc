68a830db4247f90325c72ee42a2be97b92d31789
Ernesto Mainegra-Hing <ernesto.mainegra-hing@nrc-cnrc.gc.ca>
2018-07-04 14:46:59 -0400

    Add radiative splitting ausgab object
    Add an ausgab object to turn on or off the intrinsic radiative event
    splitting available in EGSnrc, via the variable the_egsvr->nbr_split.
    Required new setRadiativeSplitting methods in the application and
    advanced application class to set the_egsvr->nbr_split accordingly.

---
+++ HEN_HOUSE/egs++/ausgab_objects/egs_radiative_splitting/egs_radiative_splitting.cpp
@@ -0,0 +1,117 @@
+/*
+###############################################################################
+#
+#  EGSnrc egs++ radiative splitting object
+#  Copyright (C) 2018 National Research Council Canada
+#
+#  This file is part of EGSnrc.
+#
+#  EGSnrc is free software: you can redistribute it and/or modify it under
+#  the terms of the GNU Affero General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+#
+#  EGSnrc is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with EGSnrc. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+#
+#  Author:          Ernesto Mainegra-Hing, 2018
+#
+#  Contributors:
+#
+###############################################################################
+#
+#  A general radiative splitting tool.
+#
+#  TODO:
+#
+#  - Add directional radiative splitting (DRS)
+#
+###############################################################################
+*/
+
+
+/*! \file egs_radiative_splitting.cpp
+ *  \brief A radiative splitting ausgab object: implementation
+ *  \EM
+ */
+
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+#include "egs_radiative_splitting.h"
+#include "egs_input.h"
+#include "egs_functions.h"
+
+EGS_RadiativeSplitting::EGS_RadiativeSplitting(const string &Name,
+        EGS_ObjectFactory *f) :
+    nsplit(1) {
+    otype = "EGS_RadiativeSplitting";
+}
+
+EGS_RadiativeSplitting::~EGS_RadiativeSplitting() {
+}
+
+void EGS_RadiativeSplitting::setApplication(EGS_Application *App) {
+    EGS_AusgabObject::setApplication(App);
+    if (!app) {
+        return;
+    }
+
+    char buf[32];
+
+    // Set EGSnrc internal radiative splitting number .
+    app->setRadiativeSplitting(nsplit);
+
+    description = "\n===========================================\n";
+    description +=  "Radiative splitting Object (";
+    description += name;
+    description += ")\n";
+    description += "===========================================\n";
+    if (nsplit > 1) {
+        description +="\n - Splitting radiative events in ";
+        sprintf(buf,"%d\n\n",nsplit);
+        description += buf;
+    }
+    else if (nsplit == 1) {
+        description +="\n - NO radiative splitting";
+    }
+    else {
+        description +="\n - BEWARE: Turning OFF radiative events !!!";
+    }
+    description += "\n===========================================\n\n";
+}
+
+//*********************************************************************
+// Process input for this ausgab object
+//
+//**********************************************************************
+extern "C" {
+
+    EGS_RADIATIVE_SPLITTING_EXPORT EGS_AusgabObject *createAusgabObject(EGS_Input *input,
+            EGS_ObjectFactory *f) {
+        const static char *func = "createAusgabObject(radiative_splitting)";
+        if (!input) {
+            egsWarning("%s: null input?\n",func);
+            return 0;
+        }
+
+        EGS_Float nsplit = 1.0;
+        int err = input->getInput("splitting",nsplit);
+
+        //=================================================
+
+        /* Setup radiative splitting object with input parameters */
+        EGS_RadiativeSplitting *result = new EGS_RadiativeSplitting("",f);
+        result->setSplitting(nsplit);
+        result->setName(input);
+        return result;
+    }
+}
