b963794f5a39e7785d8e7e761f886b32110f9cac
Elise Badun <ebadun@rogers.com>
2020-06-19 17:26:48 -0700

    Improve the dose profile plot code and display
    Add a density option, the error range, and dose labels. Also, add an
    object for dose profile dimensions and a getDoseProfileData function.

--- HEN_HOUSE/gui/dose-viewer/dose-profile.js
+++ HEN_HOUSE/gui/dose-viewer/dose-profile.js
@@ -13,30 +13,43 @@ const doseProfileButtons = d3
   });
 
 // Set the dimensions and margins of the graph
-let doseProfileFullWidth = 460;
-let doseProfileFullHeight = 400;
-var doseProfileMargin = { top: 30, right: 30, bottom: 30, left: 60 },
-  doseProfileWidth =
-    doseProfileFullWidth - doseProfileMargin.left - doseProfileMargin.right,
-  doseProfileHeight =
-    doseProfileFullHeight - doseProfileMargin.top - doseProfileMargin.bottom;
+let doseProfileDimensions = {
+  fullWidth: 510,
+  fullHeight: 440,
+  margin: { top: 30, right: 60, bottom: 60, left: 80 },
+  get width() {
+    return this.fullWidth - this.margin.left - this.margin.right;
+  },
+  get height() {
+    return this.fullHeight - this.margin.top - this.margin.bottom;
+  },
+};
 
-// var doseProfileViewer = d3.select("#dose-profile-viewer");
 var doseProfileSvg = d3
   .select("#dose-profile-svg")
-  .attr("width", doseProfileFullWidth)
-  .attr("height", doseProfileFullHeight)
+  .attr("width", doseProfileDimensions.fullWidth)
+  .attr("height", doseProfileDimensions.fullHeight)
   .append("g")
   .attr(
     "transform",
-    "translate(" + doseProfileMargin.left + "," + doseProfileMargin.top + ")"
+    "translate(" +
+      doseProfileDimensions.margin.left +
+      "," +
+      doseProfileDimensions.margin.top +
+      ")"
   );
 
-function plotDoseProfile(data, axis, coord1, coord2) {
-  //TODO: Add title, labels
-  //TODO: Clear old plot when button is repressed
+function plotDoseProfile(
+  profileSvg,
+  profileDims,
+  data,
+  axis,
+  coord1,
+  coord2,
+  densityChecked
+) {
   // Clear all existing elements
-  doseProfileSvg.selectAll("*").remove();
+  profileSvg.selectAll("*").remove();
 
   let [minPos, maxPos] = [data[0].position, data[data.length - 1].position];
   let maxDose = Math.max(...data.map((v) => v.value));
@@ -45,36 +58,53 @@ function plotDoseProfile(data, axis, coord1, coord2) {
   let xScale = d3
     .scaleLinear()
     .domain([minPos, maxPos])
-    .range([0, doseProfileWidth]);
+    .range([0, profileDims.width]);
 
-  let yScale = d3
+  let yDoseScale = d3
     .scaleLinear()
     .domain([0, maxDose])
-    .range([doseProfileHeight, 0]);
+    .range([profileDims.height, 0]);
 
   // Create and append x and y axes
   let xAxis = d3.axisBottom().scale(xScale);
-  let yAxis = d3.axisLeft().scale(yScale).tickFormat(d3.format(".2e"));
+  let yDoseAxis = d3.axisLeft().scale(yDoseScale).tickFormat(d3.format(".2e"));
 
-  doseProfileSvg
+  profileSvg
     .append("g")
     .attr("class", "x-axis")
-    .attr("transform", "translate(0," + doseProfileHeight + ")")
+    .attr("transform", "translate(0," + profileDims.height + ")")
     .call(xAxis);
 
-  doseProfileSvg.append("g").attr("class", "y-axis").call(yAxis);
+  profileSvg.append("g").attr("class", "y-dose-axis").call(yDoseAxis);
 
-  // Plot the data
+  let plotArea = profileSvg
+    .append("g")
+    .attr("width", profileDims.fullWidth)
+    .attr("height", profileDims.fullHeight)
+    .attr("fill", "none");
+
+  // Create the dose error area
+  let errorArea = d3
+    .area()
+    .x((d) => xScale(d.position))
+    .y0((d) => yDoseScale(d.value * (1.0 - parseFloat(d.err))))
+    .y1((d) => yDoseScale(d.value * (1.0 + parseFloat(d.err))));
+
+  // Create the dose line
   let line = d3
     .line()
     .x((d) => xScale(d.position))
-    .y((d) => yScale(d.value));
+    .y((d) => yDoseScale(d.value));
 
-  doseProfileSvg
-    .append("g")
-    .attr("width", doseProfileFullWidth)
-    .attr("height", doseProfileFullHeight)
-    .attr("fill", "none")
+  // Plot error
+  plotArea
+    .append("path")
+    .datum(data)
+    .attr("fill", "lightblue")
+    .attr("d", errorArea);
+
+  // Plot dose
+  plotArea
     .append("path")
     .datum(data)
     .attr("fill", "none")
@@ -84,13 +114,94 @@ function plotDoseProfile(data, axis, coord1, coord2) {
     .attr("stroke-linecap", "round")
     .attr("d", line);
 
+  // Label for position x axis
+  profileSvg
+    .append("text")
+    .attr(
+      "transform",
+      "translate(" +
+        profileDims.width / 2 +
+        " ," +
+        (profileDims.height + profileDims.margin.top + 20) +
+        ")"
+    )
+    .style("text-anchor", "middle")
+    .text(axis + " Position (cm)");
+
+  // Label for dose y axis
+  profileSvg
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr(
+      "transform",
+      "translate(" +
+        (15 - profileDims.margin.left) +
+        " ," +
+        profileDims.height / 2 +
+        ") rotate(-90)"
+    )
+    .style("text-anchor", "middle")
+    .text("Dose (Gy)");
+
+  if (densityChecked) {
+    let maxDensity = Math.max(...data.map((v) => v.density));
+
+    let yDensityScale = d3
+      .scaleLinear()
+      .domain([0, maxDensity])
+      .range([profileDims.height, 0]);
+
+    let yDensityAxis = d3
+      .axisRight()
+      .scale(yDensityScale)
+      .tickFormat(d3.format(".2f"));
+
+    profileSvg
+      .append("g")
+      .attr("class", "y-density-axis")
+      .attr("transform", "translate(" + profileDims.width + " ,0)")
+      .call(yDensityAxis);
+
+    // Create the density line
+    let densityLine = d3
+      .line()
+      .x((d) => xScale(d.position))
+      .y((d) => yDensityScale(d.density));
+
+    // Plot density
+    plotArea
+      .append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "red")
+      .attr("stroke-width", 1.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("d", densityLine);
+
+    // Label for density y axis
+    profileSvg
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr(
+        "transform",
+        "translate(" +
+          (profileDims.width + 45) +
+          " ," +
+          profileDims.height / 2 +
+          ") rotate(90)"
+      )
+      .style("text-anchor", "middle")
+      .text("Density (g/cm^2)");
+  }
+
   let [dim1, dim2] =
     axis === "x" ? ["y", "z"] : axis === "y" ? ["x", "z"] : ["x", "y"];
 
-  doseProfileSvg
+  profileSvg
     .append("text")
-    .attr("x", doseProfileWidth / 2)
-    .attr("y", 0 - doseProfileMargin.top / 2)
+    .attr("x", profileDims.width / 2)
+    .attr("y", 0 - profileDims.margin.top / 2)
     .attr("text-anchor", "middle")
     .style("font-size", "16px")
     .style("text-decoration", "underline")
@@ -129,9 +240,22 @@ function updateCoordInputsLabels(profileAxis, voxelNumber) {
 }
 
 function enableCoordInputs(voxelNumber) {
-  let coordInputs = d3.selectAll("input[name='coord-input']").nodes();
+  // Enable profile-axis radio buttons if disabled
+  let doseProfileAxisButtons = d3
+    .selectAll("input[name='profile-axis']")
+    .nodes();
+  doseProfileAxisButtons.forEach((radioButton) => {
+    if (radioButton.disabled) radioButton.disabled = false;
+  });
+
+  // Enable dose profile button if disabled
+  let doseProfileMakeButton = d3
+    .select("button[id='dose-profile-button']")
+    .node();
+  if (doseProfileMakeButton.disabled) doseProfileMakeButton.disabled = false;
 
   // Enable inputs if disabled
+  let coordInputs = d3.selectAll("input[name='coord-input']").nodes();
   coordInputs.forEach((coordInput) => {
     if (coordInput.disabled) coordInput.disabled = false;
   });
@@ -141,6 +265,67 @@ function enableCoordInputs(voxelNumber) {
     .value;
 
   updateCoordInputsLabels(profileAxis, voxelNumber);
+
+  if (!densityVol.isEmpty()) {
+    enableCheckboxForDensityPlot();
+  }
+}
+
+function enableCheckboxForDensityPlot() {
+  densityCheckbox = d3.select("input[name='density-profile-checkbox']").node();
+  if (densityCheckbox.disabled) densityCheckbox.disabled = false;
+}
+
+function getDoseProfileData(profileAxis, coord1, coord2, densityChecked) {
+  let [dim1, dim2, dim3] =
+    profileAxis === "x"
+      ? ["x", "y", "z"]
+      : profileAxis === "y"
+      ? ["y", "x", "z"]
+      : ["z", "x", "y"];
+
+  let totalSlices = parseInt(doseVol.data.voxelNumber[dim1]);
+  let position = doseVol.data.voxelArr[dim1].slice();
+  let xVoxels = parseInt(doseVol.data.voxelNumber[dim2]);
+  let yVoxels = parseInt(doseVol.data.voxelNumber[dim3]);
+
+  // Process position to get centre voxel position rather than boundaries
+  position.map((val, i) => {
+    return val + (position[i + 1] - val) / 2;
+  });
+  position.pop();
+
+  let doseProfileData = new Array(totalSlices);
+
+  for (let i = 0; i < totalSlices; i++) {
+    let address;
+    if (profileAxis === "z") {
+      address = coord1 + xVoxels * (coord2 + i * yVoxels);
+    } else if (profileAxis === "x") {
+      address =
+        i + parseInt(doseVol.data.voxelNumber.x) * (coord1 + coord2 * xVoxels);
+    } else if (profileAxis === "y") {
+      address =
+        coord1 + xVoxels * (i + coord2 * parseInt(doseVol.data.voxelNumber.y));
+    }
+
+    if (densityChecked) {
+      doseProfileData[i] = {
+        position: position[i],
+        value: doseVol.data.dose[address] || 0,
+        err: doseVol.data.error[address] || 0,
+        density: densityVol.data.density[address],
+      };
+    } else {
+      doseProfileData[i] = {
+        position: position[i],
+        value: doseVol.data.dose[address] || 0,
+        err: doseVol.data.error[address] || 0,
+      };
+    }
+  }
+
+  return doseProfileData;
 }
 
 d3.select("#dose-profile-button").on("click", function () {
@@ -149,53 +334,26 @@ d3.select("#dose-profile-button").on("click", function () {
     .value;
   let coord1 = parseInt(d3.select("#coord-1").node().value);
   let coord2 = parseInt(d3.select("#coord-2").node().value);
+  let densityChecked = d3
+    .select("input[name='density-profile-checkbox']")
+    .node().checked;
 
-  // TODO: Add checkboxes to optionally plot density if given
-  let data = doseVol.data;
+  let doseProfileData = getDoseProfileData(
+    profileAxis,
+    coord1,
+    coord2,
+    densityChecked
+  );
 
-  let totalSlices, xVoxels, yVoxels;
+  // TODO: Add a check to see if dose and density have same coordinate system
 
-  if (profileAxis === "x") {
-    totalSlices = parseInt(data.voxelNumber.x);
-    position = data.voxelArr.x.slice();
-    xVoxels = parseInt(data.voxelNumber.y);
-    yVoxels = parseInt(data.voxelNumber.z);
-  } else if (profileAxis === "y") {
-    totalSlices = parseInt(data.voxelNumber.y);
-    position = data.voxelArr.y.slice();
-    xVoxels = parseInt(data.voxelNumber.x);
-    yVoxels = parseInt(data.voxelNumber.z);
-  } else if (profileAxis === "z") {
-    totalSlices = parseInt(data.voxelNumber.z);
-    position = data.voxelArr.z.slice();
-    xVoxels = parseInt(data.voxelNumber.x);
-    yVoxels = parseInt(data.voxelNumber.y);
-  }
-
-  // Process position to get centre voxel position rather than boundaries
-  position.map((val, i) => {
-    return val + (position[i + 1] - val) / 2;
-  });
-  position.pop();
-
-  let profileDose = new Array(totalSlices);
-
-  for (let i = 0; i < totalSlices; i++) {
-    let address;
-    if (profileAxis === "z") {
-      address = coord1 + xVoxels * (coord2 + i * yVoxels);
-    } else if (profileAxis === "x") {
-      address = i + parseInt(data.voxelNumber.x) * (coord1 + coord2 * xVoxels);
-    } else if (profileAxis === "y") {
-      address = coord1 + xVoxels * (i + coord2 * parseInt(data.voxelNumber.y));
-    }
-    profileDose[i] = data.dose[address] || 0;
-  }
-
-  var doseProfileData = profileDose.map((dose, i) => ({
-    position: position[i],
-    value: dose,
-  }));
-
-  plotDoseProfile(doseProfileData, profileAxis, coord1, coord2);
+  plotDoseProfile(
+    doseProfileSvg,
+    doseProfileDimensions,
+    doseProfileData,
+    profileAxis,
+    coord1,
+    coord2,
+    densityChecked
+  );
 });
