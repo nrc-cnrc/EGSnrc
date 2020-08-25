982e1b2955d5da14b16d7e14a03f078a2a9c2ac8
SimonBiggs <me@simonbiggs.net>
2020-08-26 06:49:00 +1000

    Python can see the fortran functions

--- HEN_HOUSE/python/pyegsnrc/__init__.py
+++ HEN_HOUSE/python/pyegsnrc/__init__.py
@@ -1,4 +1,14 @@
+import importlib
+
+from ._egsnrc import *
+
 print("Hello World!")
+
+THIS = importlib.import_module(__name__)
+IMPORTABLES = dir(THIS)
+
+print("Everything that is accessible from Python:")
+print(IMPORTABLES)
 
 
 # TODO: Need to make functions like ausgab be user definable from within
