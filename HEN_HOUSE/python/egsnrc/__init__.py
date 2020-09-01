100d5abd6336006a03acfec89d197e8759a467b0
SimonBiggs <me@simonbiggs.net>
2020-09-01 20:40:58 +1000

    remove import hello world, begin notebook

--- HEN_HOUSE/python/egsnrc/__init__.py
+++ HEN_HOUSE/python/egsnrc/__init__.py
@@ -1,15 +1,4 @@
-import importlib
-import pprint
-
 from ._egsnrc import *
-
-print("Hello World!")
-
-THIS = importlib.import_module(__name__)
-IMPORTABLES = dir(THIS)
-
-print("Everything that is accessible from Python:")
-pprint.pprint(IMPORTABLES)
 
 
 # TODO: Need to make functions like ausgab be user definable from within
