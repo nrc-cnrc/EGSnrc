904e50ec8d7ec86db7a28bd0446b2f1fc97fafa5
Max Orok <maxwellorok@gmail.com>
2020-11-11 16:42:06 -0500

    EGS_Mesh: add copyright notice and header guard

--- HEN_HOUSE/egs++/geometry/egs_mesh/msh_parser.h
+++ HEN_HOUSE/egs++/geometry/egs_mesh/msh_parser.h
@@ -1,3 +1,34 @@
+/*
+###############################################################################
+#
+#  EGSnrc Gmsh msh file parser
+#  Copyright (C) 2020 National Research Council Canada
+#
+#  This file is part of EGSnrc.
+#
+#  EGSnrc is free software: you can redistribute it and/or modify it under
+#  the terms of the GNU Affero General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+#
+#  EGSnrc is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with EGSnrc. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+#
+#  Author:          Max Orok, 2020
+#
+###############################################################################
+*/
+
+#ifndef MSH_PARSER_
+#define MSH_PARSER_
+
 #include <algorithm>
 #include <fstream>
 #include <iostream>
@@ -662,3 +693,5 @@ EGS_Mesh parse_msh_file(std::istream& input) {
 }
 
 } // namespace msh_parser
+
+#endif // MSH_PARSER_
