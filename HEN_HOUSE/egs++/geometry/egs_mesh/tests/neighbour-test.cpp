ea6485e5810fa0a711b92cf8d078b4e97b7a2780
Max Orok <maxwellorok@gmail.com>
2020-11-26 16:58:48 -0500

    EGS_Mesh: copy runner output for neighbour tests

--- HEN_HOUSE/egs++/geometry/egs_mesh/tests/neighbour-test.cpp
+++ HEN_HOUSE/egs++/geometry/egs_mesh/tests/neighbour-test.cpp
@@ -100,8 +100,26 @@ int test_tetrahedron_neighbours() {
     return 0;
 }
 
+#define RUN_TEST(test_fn) \
+    std::cerr << "starting test " << #test_fn << std::endl; \
+    err = test_fn; \
+    num_total++; \
+    if (err) { \
+        std::cerr << "test FAILED" << std::endl; \
+        num_failed++; \
+    } else { \
+        std::cerr << "test passed" << std::endl; \
+    }
+
 int main() {
-    test_tetrahedron_face_eq();
-    test_tetrahedron_errors();
-    test_tetrahedron_neighbours();
+    int num_failed = 0;
+    int num_total = 0;
+    int err = 0;
+
+    RUN_TEST(test_tetrahedron_face_eq());
+    RUN_TEST(test_tetrahedron_errors());
+    RUN_TEST(test_tetrahedron_neighbours());
+
+    std::cerr << num_total - num_failed << " out of " << num_total << " tests passed\n";
+    return num_failed;
 }
