a3e81e4ab83aacc90375beecb8c1cf798383939f
Sehmimul <sehmimul777@gmail.com>
2022-04-25 12:36:33 -0400

    changed some file names

--- HEN_HOUSE/estar/modules/newtonbisec.h
+++
@@ -1,5 +0,0 @@
-#include "newtonbisec.cpp"
-
-double objective_function (double tau, double f[1000], double eps[1000], int nmax, double x);
-
-double newton_bisec(double lowerbound, double upperbound, double tolerance, double tau, double f[1000], double eps[1000], int nmax);
