fcbd83780e2e78a582e451d1f60250569b2fa5fc
Reid Townson <rwtownson@gmail.com>
2026-04-27 09:42:42 -0400

    Add contributors and copyright to estar

--- HEN_HOUSE/egs++/estar/estar_formulaCalcs.cpp
+++ HEN_HOUSE/egs++/estar/estar_formulaCalcs.cpp
@@ -1,3 +1,40 @@
+/*
+###############################################################################
+#
+#  EGSnrc estar
+#  Copyright (C) 2026 National Research Council Canada
+#
+#  This file is part of EGSnrc.
+#
+#  EGSnrc is free software: you can redistribute it and/or modify it under
+#  the terms of the GNU Affero General Public License as published by the
+#  Free Software Foundation, either version 3 of the License, or (at your
+#  option) any later version.
+#
+#  EGSnrc is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+#  FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for
+#  more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with EGSnrc. If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+#
+#  Author:          Sehmimul Hoque, 2022
+#
+#  Contributors:    Martin J. Berger
+#                   Johnathan S. Coursey
+#                   Reid Townson
+#                   Ernesto Mainegra-Hing
+#
+#  Based on the original ESTAR code by Martin J. Berger,
+#  National Institute of Standards and Technology (NIST). Including
+#  modifications by Johnathan S. Coursey.
+#
+###############################################################################
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <vector>
